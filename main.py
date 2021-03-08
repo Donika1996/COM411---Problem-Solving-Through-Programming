27a68863ae5508ab9018361b7757499250af58af
repl.it user <replituser@example.com>
2021-03-08 11:35:34 +0000

    Made a comment on EA servers

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-print("First command \git add") 
-print("Second command \EA Sports are trash")
-print("Third command \git command -m")
-print("Fourth command \git push")
-
+print ('EA Sports servers are trash') 
