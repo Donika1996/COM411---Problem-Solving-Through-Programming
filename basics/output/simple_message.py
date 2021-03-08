e20aad8d5767b10651550a0a9b92b87a9ad0d1bd
repl.it user <replituser@example.com>
2021-03-08 13:34:30 +0000

    Added messa to output

--- basics/output/simple_message.py
+++ basics/output/simple_message.py
@@ -1,1 +1,4 @@
-print("System Failure Imminent!")
+print("First command \git add")
+print("Second command \EA Sports are trash")
+print("Third command \git commit -m")
+print("Fourth command \git push")
