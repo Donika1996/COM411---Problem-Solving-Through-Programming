1ec192aed8ef29e19a4d254b9c3e8ebeceb6f7d6
repl.it user <replituser@example.com>
2021-03-26 00:00:22 +0000

    It is me again

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-#WHILE loop to print a countdown of numbers, froma a user defined value e.g. if user gives me 5, my code will print 5,4,3,2,1 BOOM
-
-print("What is the starting point?")
-start = int(input()) #control variable/counter
-start_copy = start
+import Demos.W3.roomcalc as room
 
 
-while(start>=1): #condition that needs to be satisfied for loop to continue
- print(start)
- start-=1 #Start =0
-print("BOOM!!!")
+print(room.price("bathroom", 10))
+
+while(start>=1):
+  print(start)
 
 
 
 
 
-for counter in range(start, 0, -1):
-  print(counter)
-print("BOOM!!!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
